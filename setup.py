16c4879b9335d751ecf9935c604b54d7cb18599f
Zach Fox <zachrfox@gmail.com>
2023-05-23 13:55:19 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,9 +15,6 @@ setup(
                       'gensim',                     
                       'numpy',                     
                       'tqdm',       
-                      'torch',
-                      'yaml',
-                      'gensim',
                       'torchmetrics>=0.11',                     
                       ],
 
